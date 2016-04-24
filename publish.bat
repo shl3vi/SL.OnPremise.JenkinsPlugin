0e866f50bf2a2b96c92f46503f8bc191c09a1e17
nadavye <nadav@sealights.io>
2016-04-24 10:14:26 +0300

    Remove fields from the UI

---
+++ publish.bat
@@ -0,0 +1,2 @@
+call mvn clean install -Dmaven.test.skip=true
+copy /y "C:\Work\Projects\SL.OnPremise.JenkinsPlugin\target\sealights-jenkins.hpi" "D:\Program Files (x86)\Jenkins\plugins\sealights-jenkins.hpi"
