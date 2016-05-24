b6890219fd00268a2fdbedc249a58f387ad98269
shl3vi <shahar@sealights.io>
2016-05-24 14:21:11 +0300

    Bugfix: tmp file should be deleted from master if there is no remote.

--- src/main/java/io/sealigths/plugins/sealightsjenkins/utils/FileUtils.java
+++ src/main/java/io/sealigths/plugins/sealightsjenkins/utils/FileUtils.java
@@ -89,23 +89,24 @@ public class FileUtils {
     }
 
     public static void tryDeleteFileFromSlave(Logger logger, String filename) throws IOException, InterruptedException {
+        boolean deleted;
+        logger.info("Try deleting temp file: " + filename );
+        FilePath fpOnMaster = new FilePath(new File(filename));
+        deleted = fpOnMaster.delete();
+
         if (Computer.currentComputer() instanceof SlaveComputer) {
             VirtualChannel channel = Computer.currentComputer().getChannel();
             logger.info("Current computer is: " + Computer.currentComputer().getName());
             logger.info("Jenkins current computer is: " + Jenkins.MasterComputer.currentComputer().getName());
-
             FilePath fpOnRemote = new FilePath(channel, filename);
-            FilePath fpOnMaster = new FilePath(new File(filename));
-
-            logger.info("Try deleting " + filename + " : " + fpOnRemote.absolutize());
-            boolean deleted = fpOnRemote.delete();
-            deleted = deleted && fpOnMaster.delete();
-
-            if (deleted)
-                logger.info("Deleted successfully");
-            else
-                logger.info("Not deleted");
+            deleted = deleted && fpOnRemote.delete();
         }
+
+        if (deleted)
+            logger.info("Deleted successfully.");
+        else
+            logger.info("Not deleted.");
+
     }
 
     public static boolean renameFileOrFolder(String oldName, String newName, Logger logger) {
