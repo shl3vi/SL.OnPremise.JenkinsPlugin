8f4daae679813af5a071fbfa06146693ea165072
nadavye <nadav@sealights.io>
2016-08-03 13:34:33 +0300

    Test the notification system

--- src/main/java/io/sealights/plugins/sealightsjenkins/BeginAnalysis.java
+++ src/main/java/io/sealights/plugins/sealightsjenkins/BeginAnalysis.java
@@ -161,7 +161,8 @@ public class BeginAnalysis extends Builder {
 
         if (this.executionType == null)
             this.executionType = ExecutionType.FULL;
-
+        String a = "45";
+        a+=a;
         if (this.installSealightsMavenPlugin == null)
             this.installSealightsMavenPlugin = true;
 
