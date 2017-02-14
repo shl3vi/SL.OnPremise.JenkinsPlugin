57c19291b41e706cf7da080366b13d4998bc210a
shl3vi <shahar.levi.se@gmail.com>
2017-02-14 17:51:56 +0200

    uncomment and fix tests

--- src/test/java/io/sealights/plugins/sealightsjenkins/buildsteps/cli/executors/UploadsReportsTest.java
+++ src/test/java/io/sealights/plugins/sealightsjenkins/buildsteps/cli/executors/UploadsReportsTest.java
@@ -8,11 +8,13 @@ import io.sealights.plugins.sealightsjenkins.utils.Logger;
 import io.sealights.plugins.sealightsjenkins.utils.NullLogger;
 import org.junit.Assert;
 import org.junit.Test;
+import org.mockito.ArgumentCaptor;
 
 import java.io.IOException;
 
 import static org.mockito.Matchers.any;
 import static org.mockito.Mockito.mock;
+import static org.mockito.Mockito.verify;
 import static org.mockito.Mockito.when;
 
 /**
@@ -24,30 +26,30 @@ public class UploadsReportsTest {
     private Logger nullLogger = new NullLogger();
     private boolean NO_MORE_REQUESTS = false;
 
-//    @Test
-//    public void execute_giveValidUploadReportsArguments_shouldExecuteCorrectCommand() throws IOException {
-//        //Arrange
-//        BaseCommandArguments baseCommandArguments = createBaseCommandArguments();
-//        UploadReportsCommandArguments uploadReportsArguments =
-//                new UploadReportsCommandArguments("report1.txt,report2.txt", "folders", NO_MORE_REQUESTS, "someSource");
-//        UploadReportsCommandExecutor uploadReportsExecutor = new UploadReportsCommandExecutor(nullLogger, baseCommandArguments, uploadReportsArguments);
-//
-//        Runtime runtimeMock = mock(Runtime.class);
-//
-//        final ArgumentCaptor<String> captor = ArgumentCaptor.forClass(String.class);
-//
-//        //Act
-//        uploadReportsExecutor.setRuntime(runtimeMock);
-//        uploadReportsExecutor.execute();
-//        verify(runtimeMock).exec(captor.capture());
-//        final String actualCommandLine = captor.getValue();
-//        String expectedCommandLine = "path/to/java -jar agent.jar uploadReports -token \"fake-token\" -buildsessionidfile \"/path/to/buildsessionid.txt\" -appname \"demoApp\" -buildname \"1\" -branchname \"branchy\" -environment \"someEnv\" -reportFile \"report1.txt\" -reportFile \"report2.txt\" -reportFilesFolder \"folders\" -hasMoreRequests \"false\" -source \"someSource\"";
-//
-//        // Assert
-//        Assert.assertEquals(
-//                "The command line that was executed for the 'upload reports' executor is not as expected",
-//                expectedCommandLine, actualCommandLine);
-//    }
+    @Test
+    public void execute_giveValidUploadReportsArguments_shouldExecuteCorrectCommand() throws IOException {
+        //Arrange
+        BaseCommandArguments baseCommandArguments = createBaseCommandArguments();
+        UploadReportsCommandArguments uploadReportsArguments =
+                new UploadReportsCommandArguments("report1.txt,report2.txt", "folders", NO_MORE_REQUESTS, "someSource");
+        UploadReportsCommandExecutor uploadReportsExecutor = new UploadReportsCommandExecutor(nullLogger, baseCommandArguments, uploadReportsArguments);
+
+        Runtime runtimeMock = mock(Runtime.class);
+
+        final ArgumentCaptor<String> captor = ArgumentCaptor.forClass(String.class);
+
+        //Act
+        uploadReportsExecutor.setRuntime(runtimeMock);
+        uploadReportsExecutor.execute();
+        verify(runtimeMock).exec(captor.capture());
+        final String actualCommandLine = captor.getValue();
+        String expectedCommandLine = "path/to/java -jar agent.jar uploadReports -token \"fake-token\" -buildsessionidfile \"/path/to/buildsessionid.txt\" -appname \"demoApp\" -buildname \"1\" -branchname \"branchy\" -labid \"someEnv\" -reportFile \"report1.txt\" -reportFile \"report2.txt\" -reportFilesFolder \"folders\" -hasMoreRequests \"false\" -source \"someSource\"";
+
+        // Assert
+        Assert.assertEquals(
+                "The command line that was executed for the 'upload reports' executor is not as expected",
+                expectedCommandLine, actualCommandLine);
+    }
 
     @Test
     public void execute_runtimeProcessThrowsException_shouldEndQuietly() throws IOException {
